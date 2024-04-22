697eb54bd06b48d5746fbce67fcd17acfdbe2464
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2024-04-22 20:41:29 +0300

    HeadSection

--- src/Head/Head.jsx
+++ src/Head/Head.jsx
@@ -5,8 +5,7 @@ import {
 } from "./Head.styled.js";
 
 // import ContactCenter from "../images/Contact-center.png";
-// import Ecommerce from "../images/ecommerce.png";
-// import Business from "../images/Business.png";
+
 
 const HeadSection = () => {
     return (
