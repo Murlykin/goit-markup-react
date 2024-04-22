08a9094e3323140ac8a84d21be50288c9e44ebe1
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2024-04-22 20:39:02 +0300

    HeadSection

---
+++ src/Head/Head.jsx
@@ -0,0 +1,28 @@
+import {
+    HeadSections,
+    HeadSectionsLogoColor, HeadSectionsLogo,
+
+} from "./Head.styled.js";
+
+// import ContactCenter from "../images/Contact-center.png";
+// import Ecommerce from "../images/ecommerce.png";
+// import Business from "../images/Business.png";
+
+const HeadSection = () => {
+    return (
+        <HeadSections>
+            <HeadSectionsLogo href="index.html" lang="en">
+                <HeadSectionsLogoColor>Web</HeadSectionsLogoColor >Studio</HeadSectionsLogo>
+
+
+        </HeadSections>
+    );
+};
+export default HeadSection;
+
+
+
+
+
+
+
---
+++ src/Head/Head.styled.js
@@ -0,0 +1,57 @@
+import styled from '@emotion/styled';
+export const HeadSections = styled.header`
+    background-color: var(--body-background-color);
+    border-bottom: 1px solid;
+    color: #ececec;
+`
+    ;
+export const HeadSectionsLogo = styled.a`
+    font-family: 'Raleway';
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 1.16;
+    letter-spacing: 0.03em;
+    color: var(--text-color);
+    text-decoration: none;
+    @media (min-width:1200px) {
+            font-size: 26px;
+            line-height: 1.19;
+`;
+
+export const HeadSectionsLogoColor = styled.span`
+    font-family: 'Raleway';
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 1.16;
+    letter-spacing: 0.03em;
+    color: var(--text-color);
+    text-decoration: none;
+    @media (min-width:1200px) {
+            font-size: 26px;
+            line-height: 1.19;
+     }
+`;
+
+
+
+// .conteiner-top {
+//     height: 80px;
+// }
+
+// .head-sectione__logo {
+//     font-family: 'Raleway';
+//     font-weight: 700;
+//     font-size: 24px;
+//     line-height: 1.16;
+//     letter-spacing: 0.03em;
+//     color: var(--text-color);
+//     text-decoration: none;
+//     @media (min-width:1200px) {
+//             font-size: 26px;
+//             line-height: 1.19;
+//         }
+// }
+
+// .head-sectione__logo--color {
+//     color: var(--beckground-color);
+// 
