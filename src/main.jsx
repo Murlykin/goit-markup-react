a19d460f2331f0321c93efe76997e557affe75f1
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2024-05-11 20:25:47 +0300

    misteik

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React  from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx'
 import './index.css'
-// import { HashRouter } from "react-router-dom";
-
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
