8e082f4bc921b733c119dd2e67ba1f78a10876a5
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2024-05-20 09:08:48 +0300

    return

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import './App.css'
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes, Navigate  } from 'react-router-dom';
 import { routes } from "./routes";
 import  Home  from './pages/Home';
 import  Porfolio  from "./pages/portfolio";
 import FooterSections from './Footer/index'
 
 export const App = () => {
-  const navigate = useNavigate();
   return (
     <>
-         <button onClick={() => navigate('/')}>Home</button>
-   <button onClick={() => navigate('/porfolio')}>About</button>
+      <body>
       <Routes>
         <Route path={routes.HOME} element={<Home />} />
         <Route path={routes.PORTFOLIO} element={<Porfolio />} />
-        
+        <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <FooterSections />
-     
+      </body>
     </>
   );
 };
